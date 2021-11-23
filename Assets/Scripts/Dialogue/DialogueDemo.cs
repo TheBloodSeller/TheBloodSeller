d36ab87c96aeaa473c397333412709ae6a06c55b
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-11-23 10:16:28 +0900

    Start

---
+++ Assets/Scripts/Dialogue/DialogueDemo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueDemo : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer standingImg;
+    [SerializeField] private GameObject dialogueImg;
+    [SerializeField] private Text dialogue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
