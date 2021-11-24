db1e68b6567f199ff3fbcc25896e3782da23af57
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-11-24 20:50:04 +0900

    Dialogue

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -3,28 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class DialogueText
 {
-    [TextArea]
+    public string name;
+    [TextArea]  //여러 문장을 쓸 수 있음
     public string text;
+    public Sprite cg;
 }
 
 public class Dialogue : MonoBehaviour
 {
+    public int rank;    //대화의 흐름을 알려준다
+
     [SerializeField] private SpriteRenderer standingImg;
     [SerializeField] private GameObject dialogueImg;
-    [SerializeField] private Text dialogue;
+    [SerializeField] private Text dialogueNameTxt;
+    [SerializeField] private Text dialogueTxt;
 
     private bool isDialogue = false;
     private int count = 0;
-    void Start()
+
+    [SerializeField] private DialogueText[] dialogues;
+
+    public void showDialogue()
     {
-        
+        OnOff(true);
+        count = 0;
+
+        NextDialogue();
     }
 
-    // Update is called once per frame
+   
+    void Start()
+    {
+        showDialogue();
+    }
+
     void Update()
     {
-        
+        if (isDialogue)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (count < dialogues.Length)
+                    NextDialogue();
+                else
+                    OnOff(false);
+            }
+        }
     }
+
+
+    private void NextDialogue()
+    {
+        dialogueNameTxt.text = dialogues[count].name;
+        dialogueTxt.text = dialogues[count].text;
+        standingImg.sprite = dialogues[count].cg;
+        count++;
+    }
+
+    private void OnOff(bool _flag)
+    {
+        dialogueImg.SetActive(_flag);
+        standingImg.gameObject.SetActive(_flag);
+        dialogueTxt.gameObject.SetActive(_flag);
+        isDialogue = _flag;
+    }
+
 }
