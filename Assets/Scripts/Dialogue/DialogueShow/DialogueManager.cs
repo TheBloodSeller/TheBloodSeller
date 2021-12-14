8d841c3efe75cf28f9989e53f9c8db7b125912ef
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-12-14 09:48:49 +0900

    ArgumentException 에러

--- Assets/Scripts/Dialogue/DialogueShow/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueShow/DialogueManager.cs
@@ -47,7 +47,6 @@ public class DialogueManager : MonoBehaviour
         }
 
         DisplayNextSentence();
-        
     }
 
     public void DisplayNextSentence()
--- Assets/Scripts/Dialogue/DialogueStory.cs
+++ Assets/Scripts/Dialogue/DialogueStory.cs
@@ -12,7 +12,7 @@ public class DialogueStory : MonoBehaviour
     void Generate()
     {
 
-        buttonText.Add("시작", new Dialogue("새미누리", new string[] { "2270년 어느날",
+        buttonText.Add("시작", new Dialogue("", new string[] { "2270년 어느날",
                                                               "태어난지 정확히 10000일 되는 새미누리는 오늘도 기운 없이 일어났다.",
                                                               "그녀는 세레스 행성에 갇혀있다.",
                                                               "그녀는 이 행성을 떠나기 위해 돈을 벌려 한다.",
@@ -49,7 +49,6 @@ public class DialogueStory : MonoBehaviour
 
     public void Move_Park()
     {
-        /// 싱글톤으로 보내는것은 읽기 전용인가 보다 => 새로운 방법을 알아 보자
         SystemManager.Instance.DialogueTrigger.dialogue = buttonText["공원"];
     }
 }
